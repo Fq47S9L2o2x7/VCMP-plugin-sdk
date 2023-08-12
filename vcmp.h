ce1ca6c60e760f85c6b5b48a26efc1580d41ecc4
Mark Diakhate <134564565+Fq47S9L2o2x7@users.noreply.github.com>
2023-08-11 20:34:49 -0600

    Added files to project

---
+++ vcmp.h
@@ -0,0 +1,9 @@
+// https://github.com/VCMP-SqMod/SqMod/tree/master/module/VCMP
+#pragma once
+//#define VCMP_SDK_2_1
+// Choose which SDK to use
+#ifdef VCMP_SDK_2_1
+	#include "vcmp21.h"
+#else
+	#include "vcmp20.h"
+#endif
